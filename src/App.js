bb877d3fea2c7eb0904794162eb8d68daec2e3ed
CKTIONG <95331427+CKTIONG@users.noreply.github.com>
2021-12-02 21:51:12 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// App.js
+//ref-source : https://techunderthesun.in/making-a-simple-todo-mobile-app-react-native/
+
+import * as React from 'react';
+import Main from './src/Main';
+
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
+}
